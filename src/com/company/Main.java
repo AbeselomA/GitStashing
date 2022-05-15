a111cabadd00327d5d543a459c21c37e727225ba
AbeselomA <abeselom3@gmail.com>
2022-05-15 12:03:06 -0500

    The red Audi car is ready with some bascis features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        BasicFeatures car =new BasicFeatures();
-        
+        BasicFeatures Auddi =new BasicFeatures();
+        Auddi.color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="V8";
+        Auddi.headLights="blue";
+        Auddi.regNo="tuzik";
     }
 }
